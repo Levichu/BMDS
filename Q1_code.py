2cd5ae7368a1e712282f82fb7db501e500dac89b
Levichu <98390307+Levichu@users.noreply.github.com>
2022-02-01 21:49:32 +0800

    Add files via upload

---
+++ Q1_code.py
@@ -0,0 +1,62 @@
+from tensorflow.keras import layers
+from tensorflow.keras import models
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.summary()
+
+from tensorflow.keras import optimizers
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+image_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = image_datagen.flow_from_directory(
+    train_dir,             
+    target_size=(150, 150), 
+    batch_size=20,          
+    class_mode='binary')
+
+validation_generator = image_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+test_generator = image_datagen.flow_from_directory(
+    test_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=1000,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=200)
+
+import util2 as u
+
+u.plot(history.history, ('acc','val_acc'),
+       'Train Accuracy & Validation Accuracy', 
+       ('Epochs', 'Acc & Val Acc'))
+
+test_loss, test_acc = model.evaluate_generator(train_generator)
+print('test_acc:', test_acc)
